793665515189669a8ccd6c543f223c718109effa
Nicholas Dunnaway <nick@dunnaway.org>
2021-07-22 22:15:17 -0700

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Digitalroot.Valheim.ModTemplate
-Template for Valheim Mods
+# Digitalroot.Valheim.Common.Names
+Names for Valheim Mods
+
+[![Release Drafter](https://github.com/Digitalroot-Valheim/Digitalroot.Valheim.Common.Names/actions/workflows/drafter.yml/badge.svg)](https://github.com/Digitalroot-Valheim/Digitalroot.Valheim.Common.Names/actions/workflows/drafter.yml)
+[![dotnet package](https://github.com/Digitalroot-Valheim/Digitalroot.Valheim.Common.Names/actions/workflows/publish.yml/badge.svg)](https://github.com/Digitalroot-Valheim/Digitalroot.Valheim.Common.Names/actions/workflows/publish.yml)
---
+++ src/Digitalroot.Valheim.Common.Names/PrefabNames.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Digitalroot.Valheim.Common
+{
+  public static class PrefabNames
+  {
+    public static readonly IEnumerable<string> AllNames = Utils.AllNames(typeof(PrefabNames));
+    public static readonly string Cauldron = "piece_cauldron";
+    public static readonly string DraugrElite = "Draugr_Elite";
+    public static readonly string Fenring = nameof(Fenring);
+    public static readonly string FirePit = "fire_pit";
+    public static readonly string ForceField = nameof(ForceField);
+    public static readonly string GoblinBrute = nameof(GoblinBrute);
+    public static readonly string GoblinShaman = nameof(GoblinShaman);
+    public static readonly string Munin = nameof(Munin);
+    public static readonly string Odin = "odin";
+    public static readonly string Troll = nameof(Troll);
+  }
+}
