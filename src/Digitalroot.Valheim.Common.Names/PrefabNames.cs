b8e998464b31dec69cf65a0a40f5eb1653fb2b5a
Nicholas Dunnaway <nick@dunnaway.org>
2021-07-23 17:31:48 -0700

    Added LocalizationNames

--- src/Digitalroot.Valheim.Common.Names/PrefabNames.cs
+++ src/Digitalroot.Valheim.Common.Names/PrefabNames.cs
@@ -16,4 +16,44 @@ namespace Digitalroot.Valheim.Common.Names
     public static readonly string Odin = "odin";
     public static readonly string Troll = nameof(Troll);
   }
+
+  public static class LocalizationNames
+  {
+    public static readonly IEnumerable<string> AllNames = Utils.AllNames(typeof(PrefabNames));
+    public static readonly string English = nameof(English);
+    public static readonly string Abenaki = nameof(Abenaki);
+    public static readonly string Bulgarian = nameof(Bulgarian);
+    public static readonly string Chinese = nameof(Chinese);
+    public static readonly string Croatian = nameof(Croatian);
+    public static readonly string Czech = nameof(Czech);
+    public static readonly string Danish = nameof(Danish);
+    public static readonly string Dutch = nameof(Dutch);
+    public static readonly string Estonian = nameof(Estonian);
+    public static readonly string Finnish = nameof(Finnish);
+    public static readonly string French = nameof(French);
+    public static readonly string Georgian = nameof(Georgian);
+    public static readonly string German = nameof(German);
+    public static readonly string Greek = nameof(Greek);
+    public static readonly string Hindi = nameof(Hindi);
+    public static readonly string Hungarian = nameof(Hungarian);
+    public static readonly string Icelandic = nameof(Icelandic);
+    public static readonly string Italian = nameof(Italian);
+    public static readonly string Japanese = nameof(Japanese);
+    public static readonly string Korean = nameof(Korean);
+    public static readonly string Lithuanian = nameof(Lithuanian);
+    public static readonly string Macedonian = nameof(Macedonian);
+    public static readonly string Norwegian = nameof(Norwegian);
+    public static readonly string Polish = nameof(Polish);
+    public static readonly string Portuguese = nameof(Portuguese);
+    public static readonly string PortugueseBrazilian = "Portuguese_Brazilian";
+    public static readonly string Romanian = nameof(Romanian);
+    public static readonly string Russian = nameof(Russian);
+    public static readonly string Serbian = nameof(Serbian);
+    public static readonly string Slovak = nameof(Slovak);
+    public static readonly string Spanish = nameof(Spanish);
+    public static readonly string Swedish = nameof(Swedish);
+    public static readonly string Thai = nameof(Thai);
+    public static readonly string Turkish = nameof(Turkish);
+    public static readonly string Ukrainian = nameof(Ukrainian);
+  }
 }
