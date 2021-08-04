0b67329c158e78630235d09eae095f3cacddb4d9
Nicholas Dunnaway <nick@dunnaway.org>
2021-08-03 20:37:00 -0700

    Added SpawnThat NPCs

---
+++ src/Digitalroot.Valheim.Common.Names/SpawnThatNPCNames.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Digitalroot.Valheim.Common.Names
+{
+  [SuppressMessage("ReSharper", "InconsistentNaming")]
+  public static partial class SpawnThatNPCNames
+  {
+    public static readonly IEnumerable<string> AllNames = Utils.AllNames(typeof(PrefabNames));
+
+    public static readonly string Freki = nameof(Freki);
+    public static readonly string Geri = nameof(Geri);
+    public static readonly string Groot = nameof(Groot);
+    public static readonly string MiniSkelly = nameof(MiniSkelly);
+    public static readonly string MiniSquito = nameof(MiniSquito);
+    public static readonly string Trundle = nameof(Trundle);
+    public static readonly string DarkWolf = nameof(DarkWolf);
+    public static readonly string LightWolf = nameof(LightWolf);
+  }
+}
