6bd90134c3f71d0192774bb815794f1652e08003
Digitalroot <Digitalroot@users.noreply.github.com>
2021-08-26 17:53:37 -0700

    Update RRRMBossNames.cs

--- src/Digitalroot.Valheim.Common.Names/RRRMBossNames.cs
+++ src/Digitalroot.Valheim.Common.Names/RRRMBossNames.cs
@@ -21,19 +21,19 @@ namespace Digitalroot.Valheim.Common.Names
     public static readonly string mountainbandit3 = $"RRRN_{nameof(mountainbandit3)}";
     public static readonly string plainsbandit3 = $"RRRN_{nameof(plainsbandit3)}";
     public static readonly string wildbandit3 = $"RRRN_{nameof(wildbandit3)}";
-    public static readonly string IceGolem = nameof(IceGolem);
-    public static readonly string EikthyrLox = nameof(EikthyrLox);
-    public static readonly string EikthyrNeckT3M = nameof(EikthyrNeckT3M);
-    public static readonly string TrollT2Elite = nameof(TrollT2Elite);
-    public static readonly string TrollT3Elite = nameof(TrollT3Elite);
-    public static readonly string TrollT4Elite = nameof(TrollT4Elite);
-    public static readonly string GDElderBrute = nameof(GDElderBrute);
-    public static readonly string EikthyrNeckT4 = nameof(EikthyrNeckT4);
-    public static readonly string GDBurningTorch = nameof(GDBurningTorch);
-    public static readonly string UndeadHrungnir = nameof(UndeadHrungnir);
-    public static readonly string SkeletonT3Captain = nameof(SkeletonT3Captain);
-    public static readonly string SkeletonT4Captain = nameof(SkeletonT4Captain);
-    public static readonly string WraithT3 = nameof(WraithT3);
+    public static readonly string IceGolem = $"{Prefix}{nameof(IceGolem)}";
+    public static readonly string EikthyrLox = $"{Prefix}{nameof(EikthyrLox)}";
+    public static readonly string EikthyrNeckT3M = $"{Prefix}{nameof(EikthyrNeckT3M)}";
+    public static readonly string TrollT2Elite = $"{Prefix}{nameof(TrollT2Elite)}";
+    public static readonly string TrollT3Elite = $"{Prefix}{nameof(TrollT3Elite)}";
+    public static readonly string TrollT4Elite = $"{Prefix}{nameof(TrollT4Elite)}";
+    public static readonly string GDElderBrute = $"{Prefix}{nameof(GDElderBrute)}";
+    public static readonly string EikthyrNeckT4 = $"{Prefix}{nameof(EikthyrNeckT4)}";
+    public static readonly string GDBurningTorch = $"{Prefix}{nameof(GDBurningTorch)}";
+    public static readonly string UndeadHrungnir = $"{Prefix}{nameof(UndeadHrungnir)}";
+    public static readonly string SkeletonT3Captain = $"{Prefix}{nameof(SkeletonT3Captain)}";
+    public static readonly string SkeletonT4Captain = $"{Prefix}{nameof(SkeletonT4Captain)}";
+    public static readonly string WraithT3 = $"{Prefix}{nameof(WraithT3)}";
 
     [UsedImplicitly]
     public static IEnumerable<string> AllNamesByBiome(Heightmap.Biome biome)
