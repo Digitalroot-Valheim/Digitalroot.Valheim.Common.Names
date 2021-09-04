bcc4c77f5f9f594f4a6725729e42357a22a67fc4
Nicholas Dunnaway <nick@dunnaway.org>
2021-09-04 13:06:26 -0700

    Added Craftion Stations

---
+++ src/Digitalroot.Valheim.Common.Names/CraftingStationNames.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Digitalroot.Valheim.Common.Names
+{
+  public static partial class CraftingStationNames
+  {
+    public static readonly IEnumerable<string> AllNames = Utils.AllNames(typeof(PrefabNames));
+    public static readonly string ArtisanTable = "piece_artisanstation";
+    public static readonly string Cauldron = "piece_cauldron";
+    public static readonly string Forge = "forge";
+    public static readonly string Stonecutter = "piece_stonecutter";
+    public static readonly string Workbench = "piece_workbench";
+  }
+}
